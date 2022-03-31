48c3de74ae10870140838c98dec26d6c95e097e8
Lucas Alf <lucasmachadoalf@gmail.com>
2022-03-30 22:15:07 -0300

    Ajustes na home page.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,31 @@
 import React from "react";
-
-import styles from "./styles.module.css";
+import Container from "@mui/material/Container";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Typography from "@mui/material/Typography";
+import ListItemText from "@mui/material/ListItemText";
 
 function Home() {
-  return <h1>Hello Home page!</h1>;
+  return (
+    <>
+      <Container maxWidth="sm">
+        <List>
+          <ListItem alignItems="flex-start">
+            <ListItemText
+              primary="Pegunta 1"
+              secondary={<>{"Texto da pergunta 1..."}</>}
+            />
+          </ListItem>
+          <ListItem alignItems="flex-start">
+            <ListItemText
+              primary="Pegunta "
+              secondary={<>{"Texto da pergunta 2..."}</>}
+            />
+          </ListItem>
+        </List>
+      </Container>
+    </>
+  );
 }
 
 export default Home;
