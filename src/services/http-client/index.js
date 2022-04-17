bb1d7af41075fb081f2d43c5222a6282cf19ab11
Lucas Alf <lucasmachadoalf@gmail.com>
2022-04-16 23:46:00 -0300

    Ajuste na URL da API de produção.

--- src/services/http-client/index.js
+++ src/services/http-client/index.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL:
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+      ? "http://localhost:5000"
+      : "https://help-desker-api.herokuapp.com",
 });
 
 const httpGet = async (url, data, params = {}) => {
